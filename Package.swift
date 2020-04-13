31e4786ad5a2c3332133806e283af2f5f2578956
Michael Critz <github@michaelcritz.com>
2020-04-12 20:50:08 -0700

    add leaf

--- Package.swift
+++ Package.swift
@@ -10,7 +10,8 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc"),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0-rc")
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0-rc"),
+        .package(url: "https://github.com/vapor/leaf.git", .branch("master")),
     ],
     targets: [
         .target(name: "App", dependencies: [
